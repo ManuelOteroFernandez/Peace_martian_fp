727751b4993e1251e628a641db6bd21f7b17c019
Vilas <chouza.vilas@gmail.com>
2025-02-22 19:07:34 +0100

    Migración al nuevo Input System. Ajuste de la entrada para el correcto uso de teclado+ratón o controlador

--- Assets/Scripts/Player/PlayerInputController.cs
+++ Assets/Scripts/Player/PlayerInputController.cs
@@ -1,72 +1,98 @@
+using System.Numerics;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using Vector2 = UnityEngine.Vector2;
 
 public class PlayerInputController : MonoBehaviour
 {
+    PlayerInputActions inputActions;
+
     [Header("Input")]
-    public float horizontalInput {get; private set;} //Joystick izquierdo
-    public bool jumpInput {get; private set;} //X
-    public bool dashInput {get; private set;} //L2
-    public bool nextWeaponInput {get; private set;} //Cruceta derecha
-    public bool previousWeaponInput {get; private set;} //Cruceta izquierda
-    public bool shootInput {get; private set;} //R2
-    public bool shootHoldInput {get; private set;} //R2
-    public Vector2 aimDirection {get; private set;} //Joystick derecho - Ratón
+    public float horizontalInput {get; private set;}
+    public bool jumpInput {get; private set;}
+    public bool dashInput {get; private set;}
+    public bool nextWeaponInput {get; private set;}
+    public bool previousWeaponInput {get; private set;}
+    public bool shootInput {get; private set;}
+    public bool shootHoldInput {get; private set;}
+    public Vector2 aimDirection {get; private set;}    
 
     private bool forceRelease = false;
+    private bool isUsingJoystick = false;
 
     [Header("Jump Buffer")]
     [SerializeField] private float jumpBufferTime;
     private float jumpBufferCounter;
 
     [Header("Joystick Deadzone")]
-    [SerializeField] private float joystickDeadzone;
+    [SerializeField] private float joystickDeadzone = 0.3f;
 
+    private void OnEnable() => inputActions.Enable();
+    private void OnDisable() => inputActions.Disable();
 
-    void Update() {
-        horizontalInput = Input.GetAxisRaw("Horizontal");
+    private void Awake() {
+        inputActions = new PlayerInputActions();
 
-        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        aimDirection = (mousePosition - (Vector2)transform.position).normalized;
+        inputActions.Player.Move.performed += ctx => {
+            Vector2 moveInput = ctx.ReadValue<Vector2>();
 
-        /*Vector2 joystickInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+            horizontalInput = moveInput.x != 0 ? Mathf.Sign(moveInput.x) : 0;
+        };
 
-        if (joystickInput.magnitude < joystickDeadzone) {
-            aimDirection = joystickInput.normalized;
-        }*/
+        inputActions.Player.Move.canceled += ctx => horizontalInput = 0;
 
-        if (Input.GetButtonDown("Jump")) {
-            jumpInput = true;
+        inputActions.Player.AimJoystick.performed += ctx => {
+            Vector2 joystickInput = ctx.ReadValue<Vector2>();
+
+            //Se aplica un deadzone al joystick para evitar valores residuales que no filtra el propio Input System
+            if (joystickInput.magnitude > joystickDeadzone) 
+            {
+                aimDirection = joystickInput.normalized;
+                isUsingJoystick = true;
+            }
+        };
+
+        inputActions.Player.AimJoystick.canceled += ctx => {
+            aimDirection = Vector2.zero;
+            isUsingJoystick = false;
+        };
+
+        inputActions.Player.AimMouse.performed += ctx =>
+        {
+            if (!isUsingJoystick) {
+                Vector2 mousePosition = Camera.main.ScreenToWorldPoint(ctx.ReadValue<Vector2>());
+                aimDirection = (mousePosition - (Vector2)transform.position).normalized;
+            }
+        };
+
+        inputActions.Player.Jump.performed += ctx => jumpInput = true;
+
+        inputActions.Player.Dash.performed += ctx => dashInput = true;
+
+        inputActions.Player.Shoot.performed += ctx => shootInput = true;
+
+        inputActions.Player.Shoot.performed += ctx => shootHoldInput = true;
+        inputActions.Player.Shoot.canceled += ctx => shootHoldInput = false;
+
+        inputActions.Player.NextWeapon.performed += ctx => nextWeaponInput = true;
+        inputActions.Player.PreviousWeapon.performed += ctx => previousWeaponInput = true;
+    }
+
+    private void Update() {
+        //Se aplica un buffer al salto para que el usuario no sienta que se ignoran sus controles.
+        if (jumpInput) {
             jumpBufferCounter = jumpBufferTime;
         } else {
             jumpBufferCounter -= Time.deltaTime;
         }
 
-        if (Input.GetButtonDown("Dash")) {
-            dashInput = true;
-        }
-
-        if (Input.GetButtonDown("NextWeapon")) {
-            nextWeaponInput = true;
-        }
-
-        /*if (Input.GetButtonDown("PreviousWeapon")) {
-            previousWeaponInput = true;
-        }*/
-    
-        if (Input.GetButtonDown("Fire1")) {
-            shootInput = true;
-        }
-
-        //Mecanismo para forzar al jugador a soltar el botón de disparo antes de poder volver a disparar
-        bool isHoldingShoot = Input.GetButton("Fire1");
+        //Se fuerza al jugador a volver a disparar
         if (forceRelease) {
-            if (!isHoldingShoot) {
+            if (!shootHoldInput) {
                 forceRelease = false;
             }
 
             shootHoldInput = false;
-        } else {
-            shootHoldInput = isHoldingShoot;
         }
     }
 
@@ -86,7 +112,7 @@ public class PlayerInputController : MonoBehaviour
         dashInput = false;
         shootInput = false;
         nextWeaponInput = false;
-        //previousWeaponInput = false;
+        previousWeaponInput = false;
     }
 
     public void ForceRelease() {
