4acd3cef3e441e62c73fd7da473cb207094bb382
Rykmar <chouza.vilas@gmail.com>
2025-03-25 18:52:59 +0100

    Import del paquete para la localización y creación de ejemplo

--- Assets/Scripts/Testing/TestingUtils.cs
+++ Assets/Scripts/Testing/TestingUtils.cs
@@ -1,5 +1,6 @@
 using UnityEditor.SearchService;
 using UnityEngine;
+using UnityEngine.Localization.Settings;
 using UnityEngine.SceneManagement;
 
 public class TestingUtils : MonoBehaviour
@@ -21,6 +22,18 @@ public class TestingUtils : MonoBehaviour
     {
         if (Input.GetKeyDown("p")) {
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+
+        if (Input.GetKeyDown("1")) {
+            LocalizationSettings.SelectedLocale = LocalizationSettings.AvailableLocales.Locales[0];
+        }
+
+        if (Input.GetKeyDown("2")) {
+            LocalizationSettings.SelectedLocale = LocalizationSettings.AvailableLocales.Locales[1];
+        }
+
+        if (Input.GetKeyDown("3")) {
+            LocalizationSettings.SelectedLocale = LocalizationSettings.AvailableLocales.Locales[2];
         }
     }
 }
