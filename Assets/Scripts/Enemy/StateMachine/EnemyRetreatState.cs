5ecb0026fd114df7ac46d55f3790facdf1d9a8d2
Rykmar <chouza.vilas@gmail.com>
2025-04-01 19:19:19 +0200

    Correcciones en IA de enemigos

---
+++ Assets/Scripts/Enemy/StateMachine/EnemyRetreatState.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class EnemyRetreatState : EnemyState
+{
+    public override void Enter(EnemyController controller)
+    {
+        //controller.animator.SetTrigger("isRunning");
+    }
+
+    public override void Update(EnemyController controller)
+    {
+        controller.ReturnToOrigin();
+
+        if (controller.IsBackInOrigin()) {
+            controller.ChangeState(new EnemyPatrolState());
+        }
+        // controller.MoveEnemy();
+
+        // if (controller.enemyHealth.IsInBubble()) {
+        //     controller.ChangeState(new EnemyBubbleTrappedState());
+        // }
+
+        // if (controller.rigidbody2D.linearVelocityY < 0) {
+        //     controller.ChangeState(new EnemyFallingState());
+        // }
+
+        // if (controller.DistanceToOriginWaypoint() < 0.5f) {
+        //     controller.ChangeState(new EnemyPatrolState());
+        // }
+
+        // if (controller.IsTrappedEnemyInAttackRange() || (controller.IsTargetInAttackRange() && controller.IsGrounded() && controller.IsAttackCooldownReady())) {
+        //     controller.ChangeState(new EnemyAttackState());
+        // }
+    }
+
+    public override void Exit(EnemyController controller)
+    {
+        //controller.animator.ResetTrigger("isRunning");
+    }
+}
